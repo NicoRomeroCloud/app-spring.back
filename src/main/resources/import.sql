1d42a1fdd447835ed608e7ef2cfa274ba3b7f149
Nicolas Romero <nicolas.romero1701@alumnos.ubiobio.cl>
2023-10-24 15:53:17 -0300

    Tests faltantes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,13 +1,16 @@
 -- Inserción de marcas
-INSERT INTO marcas(nombre, descripcion) VALUES ('BMW', 'Deportivo');
-INSERT INTO marcas(nombre, descripcion) VALUES ('Audi', 'Sedán de lujo');
-INSERT INTO marcas(nombre, descripcion) VALUES ('Tesla', 'Vehículo eléctrico');
+INSERT INTO marcas(nombre, descripcion) VALUES ('BMW', 'El BMW Isetta es un mini vehículo construido en 1955');
+INSERT INTO marcas(nombre, descripcion) VALUES ('Audi', 'Aerodinámicamente pensado al detalle, muestra su carácter único gracias a los exclusivos detalles RS. El Audi RS 3 Sportback en color rojo tango metalizado.');
+INSERT INTO marcas(nombre, descripcion) VALUES ('Tesla', 'La berlina eléctrica de los ¡más de 1.000 CV! Ya se puede encargar el Model S más salvaje hasta la fecha, con unas prestaciones de infarto.');
+INSERT INTO marcas(nombre, descripcion) VALUES ('Fiat', 'Versión Ejecutiva, cuenta con pisadera eléctrica, iluminación de interior led, butacas y además se puede instalar equipamiento adicionales como luces de cortesía en el piso interior.');
+INSERT INTO marcas(nombre, descripcion) VALUES ('Foton', '"GRAN DISEÑO Y CAPACIDAD DE CARGA" Está diseñada para ser tu mejor aliado al transportar tus productos tanto en la ciudad o en zonas rurales.');
+INSERT INTO marcas(nombre, descripcion) VALUES ('Ford', 'La nueva Maverick Híbrida, equipa un avanzado nivel de tecnología aplicada a la seguridad de los ocupantes y de terceros.');
 
 -- Inserción de autos relacionados con las marcas
-INSERT INTO autos (precio, nombre, marca_id) VALUES (150000, 'BMW X5', 3);
-INSERT INTO autos (precio, nombre, marca_id) VALUES (80000, 'Audi A6', 1);
-INSERT INTO autos (precio, nombre, marca_id) VALUES (60000, 'Tesla Model 3', 2);
-INSERT INTO autos (precio, nombre, marca_id) VALUES (35000, 'BMW 3 Series', 3);
-INSERT INTO autos (precio, nombre, marca_id) VALUES (75000, 'Audi Q5',1);
-INSERT INTO autos (precio, nombre, marca_id) VALUES (45000, 'Tesla Model Y', 2);
+INSERT INTO autos (precio, nombre, marca_id) VALUES (150000, 'BMW Isetta', 1);
+INSERT INTO autos (precio, nombre, marca_id) VALUES (80000, 'RS 3 Sportback', 2);
+INSERT INTO autos (precio, nombre, marca_id) VALUES (60000, 'Tesla Model S Plaid', 3);
+INSERT INTO autos (precio, nombre, marca_id) VALUES (35000, 'DUCATO PASAJEROS', 4);
+INSERT INTO autos (precio, nombre, marca_id) VALUES (75000, 'Midi cargo refrigerado', 5);
+INSERT INTO autos (precio, nombre, marca_id) VALUES (45000, 'Maverick Híbrida', 6);
 
--- src/test/java/com/springboot/angular/backend/apirest/SpringBootBackendApirestApplicationTests.java
+++ src/test/java/com/springboot/angular/backend/apirest/SpringBootBackendApirestApplicationTests.java
@@ -1,10 +1,250 @@
 package com.springboot.angular.backend.apirest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.angular.backend.apirest.entity.Auto;
+import com.springboot.angular.backend.apirest.entity.Marca;
+import com.springboot.angular.backend.apirest.repository.IAutoRepository;
+import com.springboot.angular.backend.apirest.repository.IMarcaRepository;
+import com.springboot.angular.backend.apirest.services.AutoServiceImpl;
+import com.springboot.angular.backend.apirest.services.MarcaServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.mockito.ArgumentMatchers.any;
+
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.*;
+
+import java.util.*;
 
 @SpringBootTest
 class SpringBootBackendApirestApplicationTests {
+
+
+	@Mock
+	IAutoRepository autoRepository;
+
+	@Mock
+	IMarcaRepository marcaRepository;
+
+	@InjectMocks
+	AutoServiceImpl autoService;
+
+	@InjectMocks
+	MarcaServiceImpl marcaService;
+
+	Marca marca1;
+	Marca marca2;
+
+	Auto auto1;
+	Auto auto2;
+	Auto auto3;
+
+
+	@BeforeEach
+	void setup() {
+		MockitoAnnotations.openMocks(this);
+		marca1 = new Marca(Long.valueOf(1), "Mercedes", "Marca de autos de lujo", new HashSet<>());
+		marca2 = new Marca(Long.valueOf(2), "Toyota", "Marca de autos japonesa", new HashSet<>());
+		auto1 = new Auto();
+		auto1.setId(Long.valueOf(1));
+		auto1.setNombre("FordAdventure");
+		auto1.setPrecio(20000);
+		auto1.setMarca(marca1);
+		auto2 = new Auto();
+		auto2.setId(Long.valueOf(2));
+		auto2.setNombre("Adventure");
+		auto2.setPrecio(3000);
+		auto2.setMarca(marca2);
+		auto3 = new Auto();
+		auto3.setId(Long.valueOf(3));
+		auto3.setNombre("Adventure2");
+		auto3.setPrecio(40000);
+		auto3.setMarca(marca1);
+	}
+
+	@Test
+	void findAutoByIdTest() {
+		when(autoRepository.findById(Long.valueOf(1))).thenReturn(Optional.of(auto1));
+
+		Auto buscado = autoService.findById(Long.valueOf(1));
+
+		assertEquals("FordAdventure", buscado.getNombre()); // Cambiado a "FordAdventure"
+		assertEquals(20000, buscado.getPrecio()); // Cambiado a 20000
+		assertEquals(Long.valueOf(1), buscado.getId());
+		assertEquals("Mercedes", buscado.getMarca().getNombre()); // Cambiado a "Mercedes"
+
+		verify(autoRepository).findById(Long.valueOf(1));
+	}
+
+	@Test
+	void saveAutoTest() {
+		// Given
+		Auto fordAdventure = new Auto();
+		fordAdventure.setId(Long.valueOf(1));
+		fordAdventure.setNombre("FordAdventure");
+		fordAdventure.setPrecio(20000);
+		fordAdventure.setMarca(marca1);
+
+		when(autoRepository.save(any())).then(invocation -> {
+			Auto auto = invocation.getArgument(0);
+			auto.setId(Long.valueOf(3)); // Simulamos la asignación del ID
+			return auto;
+		});
+
+		// When
+		Auto autoPrueba = autoService.save(fordAdventure);
+
+		// Then
+		assertEquals(Long.valueOf(3), autoPrueba.getId()); // Verificamos que el ID sea 3
+		assertEquals("FordAdventure", autoPrueba.getNombre()); // Verificamos el nombre
+		assertEquals(20000, autoPrueba.getPrecio()); // Verificamos el precio
+		assertEquals("Mercedes", autoPrueba.getMarca().getNombre()); // Verificamos la marca
+
+		verify(autoRepository).save(any()); // Verificamos que se haya llamado a save en el repositorio
+	}
+
+
+
+	@Test
+	void findAllAutoTest() {
+		// Given
+		List<Auto> autos = new ArrayList<Auto>();
+		autos.add(auto1);
+		autos.add(auto2);
+
+		when(autoRepository.findAll()).thenReturn(autos);
+
+		// When
+		List<Auto> autosPrueba = autoService.findAll();
+
+		// Then
+		assertFalse(autosPrueba.isEmpty());
+		assertEquals(2, autosPrueba.size());
+
+		// Aquí puedes agregar aserciones para verificar los atributos de los autos y sus marcas.
+		// Por ejemplo:
+		assertEquals(Long.valueOf(1), autosPrueba.get(0).getId());
+		assertEquals("FordAdventure", autosPrueba.get(0).getNombre());
+		assertEquals(20000, autosPrueba.get(0).getPrecio());
+		assertEquals("Mercedes", autosPrueba.get(0).getMarca().getNombre());
+
+		assertEquals(Long.valueOf(2), autosPrueba.get(1).getId());
+		assertEquals("Adventure", autosPrueba.get(1).getNombre());
+		assertEquals(3000, autosPrueba.get(1).getPrecio());
+		assertEquals("Toyota", autosPrueba.get(1).getMarca().getNombre());
+
+		verify(autoRepository).findAll();
+	}
+
+	@Test
+	void findAutoByNombreTest() {
+		when(autoRepository.findByNombre(any())).thenReturn(Arrays.asList(auto1));
+
+		List<Auto> autosBuscados = autoService.findByNombre("FordAdventure");
+
+		assertFalse(autosBuscados.isEmpty());
+		assertEquals(1, autosBuscados.size());
+		assertEquals("FordAdventure", autosBuscados.get(0).getNombre());
+		assertEquals(20000, autosBuscados.get(0).getPrecio());
+		assertEquals(Long.valueOf(1), autosBuscados.get(0).getId());
+		assertEquals("Mercedes", autosBuscados.get(0).getMarca().getNombre());
+
+		verify(autoRepository).findByNombre(any());
+	}
+
+
+	@Test
+	void DeleleMarcaTest() {
+		doNothing().when(marcaRepository).deleteById(any());
+
+		marcaService.delete(Long.valueOf(1));
+
+		verify(marcaRepository).deleteById(any());
+	}
+
+
+	//_-------------------------------_________
+
+	@Test
+	void findMarcaByIdTest() {
+		when(marcaRepository.findById(Long.valueOf(1))).thenReturn(Optional.of(marca1));
+
+		Marca buscada = marcaService.findById(Long.valueOf(1));
+
+		assertEquals("Mercedes", buscada.getNombre());
+		assertEquals("Marca de autos de lujo", buscada.getDescripcion());
+		assertEquals(Long.valueOf(1), buscada.getId());
+		assertEquals(0, buscada.getAutos().size());
+
+		verify(marcaRepository).findById(Long.valueOf(1));
+	}
+
+	@Test
+	void saveMarcaTest() {
+		// Given
+		Marca mercedes = new Marca(null, "Mercedes", "Marca de autos de lujo", new HashSet<Auto>());
+		when(marcaRepository.save(any())).then(invocation -> {
+			Marca m = invocation.getArgument(0);
+			// Se setea el id de la marca nueva que venga, en este caso "Mercedes"
+			m.setId(Long.valueOf(3));
+			return m;
+		});
+
+		// When
+		Marca marcaPrueba = marcaService.save(mercedes);
+
+		// Then
+		assertEquals(Long.valueOf(3), marcaPrueba.getId());
+		assertEquals("Mercedes", marcaPrueba.getNombre());
+		assertEquals("Marca de autos de lujo", marcaPrueba.getDescripcion());
+
+		verify(marcaRepository).save(any());
+	}
+
+	@Test
+	void findAllMarcaTest() {
+		// Given
+		List<Marca> marcas = new ArrayList<Marca>();
+		marcas.add(marca1);
+		marcas.add(marca2);
+		when(marcaRepository.findAll()).thenReturn(marcas);
+
+		// When
+		List<Marca> marcasPrueba = marcaService.findAll();
+
+		// Then
+		assertFalse(marcasPrueba.isEmpty());
+		assertEquals(2, marcasPrueba.size());
+		verify(marcaRepository).findAll();
+	}
+
+	@Test
+	void findMarcaByNombreTest() {
+		List<Marca> marcas = new ArrayList<Marca>();
+		marcas.add(marca1);
+		when(marcaRepository.findByNombre(any())).thenReturn(marcas);
+
+		List<Marca> marcasBuscadas = marcaService.findByNombre("Mercedes");
+
+		assertEquals(1, marcasBuscadas.size());
+
+		verify(marcaRepository).findByNombre(any());
+	}
+
+	@Test
+	void deleteMarcaTest() {
+		doNothing().when(marcaRepository).deleteById(any());
+
+		marcaService.delete(Long.valueOf(1));
+
+		verify(marcaRepository).deleteById(any());
+	}
 
 	@Test
 	void contextLoads() {
